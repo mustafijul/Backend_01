ececdc5ab5fbd1bcd94babcd3180faec346c3e2c
mustafijul <mustafijulislam21@gmail.com>
2025-10-29 21:48:21 +0600

    forEach and Map function done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Bismillah");
+
+var arr = [1, 2, 3, 4]
+
+// forEach 
+
+// const res = arr.forEach(function(val){
+//     val = val + " hello"
+//     console.log(val);
+// })
+
+// Map
+
+// var newArr = arr.map( (num) =>{
+//     return num*2
+// })
+// console.log(newArr);
+
+
+
+
+
+
